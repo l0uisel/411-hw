938f36c9b7f54d9596f5eae8eb0005b5f86dea6a
l0uisel <lleelouise@gmail.com>
2024-10-06 17:07:50 -0400

    Sort variable and files of migrations

--- wildlife_tracker/migration_tracking/migration_manager.py
+++ wildlife_tracker/migration_tracking/migration_manager.py
@@ -6,31 +6,10 @@ class MigrationManager:
 def cancel_migration(migration_id: int) -> None:
     pass
 
-def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
-    pass
-
-def remove_migration_path(path_id: int) -> None:
-    pass
-
 def schedule_migration(migration_path: MigrationPath) -> None:
     pass
 
 def get_migration_by_id(migration_id: int) -> Migration:
-    pass
-
-def get_migration_path_by_id(path_id: int) -> MigrationPath:
-    pass
-
-def get_migration_paths() -> list[MigrationPath]:
-    pass
-
-def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
-    pass
-
-def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
-    pass
-
-def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
     pass
 
 def get_migrations() -> list[Migration]:
@@ -48,8 +27,3 @@ def get_migrations_by_start_date(start_date: str) -> list[Migration]:
 def get_migrations_by_status(status: str) -> list[Migration]:
     pass
 
-def get_migration_path_details(path_id) -> dict:
-    pass
-
-def update_migration_path_details(path_id: int, **kwargs) -> None:
-    pass
--- wildlife_tracker/migration_tracking/migration_path.py
+++ wildlife_tracker/migration_tracking/migration_path.py
@@ -2,4 +2,8 @@ from typing import Optional
 
 class MigrationPath:
 
+def get_migration_path_details(path_id) -> dict:
+    pass
+
+def update_migration_path_details(path_id: int, **kwargs) -> None:
     pass
